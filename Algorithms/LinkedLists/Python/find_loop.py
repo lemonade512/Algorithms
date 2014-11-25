ee4b9e19c62240c2bd75045c514eeb1fa8cb8963
lemonade512 <philliplemons@utexas.edu>
2014-11-25 11:14:52 -0600

    Fixed file naming conventions

--- Algorithms/LinkedLists/Python/find_loop.py
+++ Algorithms/LinkedLists/Python/find_loop.py
@@ -6,21 +6,21 @@ def HasLoopMarking(sentinel):
     has_loop = False
 
     cell = sentinel
-    while cell.next != None:
-        if cell.next.visited:
-            cell.next = None
+    while cell.next_ != None:
+        if cell.next_.visited:
+            cell.next_ = None
             print cell.value
-            print cell.next
+            print cell.next_
             has_loop = True
             break
 
         cell.visited = True
-        cell = cell.next
+        cell = cell.next_
 
     cell = sentinel
-    while cell.next != None:
+    while cell.next_ != None:
         cell.visited = False
-        cell = cell.next
+        cell = cell.next_
 
     return has_loop
 
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     c2 = IntegerCell(2)
     c3 = IntegerCell(3)
     c4 = IntegerCell(4)
-    c1.next = c2
-    c2.next = c3
-    c3.next = c4
-    c4.next = c2
+    c1.next_ = c2
+    c2.next_ = c3
+    c3.next_ = c4
+    c4.next_ = c2
     print HasLoopMarking(c1)
-    print c4.next
+    print c4.next_
