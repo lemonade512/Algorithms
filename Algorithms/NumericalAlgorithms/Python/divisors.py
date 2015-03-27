1a599a0dcf4a30d6328114f2cb9bf9768a6e8a26
lemonade512 <philliplemons@utexas.edu>
2015-03-27 13:36:11 -0500

    Cleaned the divisors function to be more readable

--- Algorithms/NumericalAlgorithms/Python/divisors.py
+++ Algorithms/NumericalAlgorithms/Python/divisors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from math import sqrt, ceil
+from math import sqrt
 
 def divisors(n):
     """ Calculates the divisors of `n` and returns the list.
@@ -15,16 +15,17 @@ def divisors(n):
     Example:
         >>> divisors(28)
         [1, 2, 4, 7, 14, 28]
+        >>> divisors(144)
+        [1, 2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 36, 48, 72, 144]
 
     """
     output = list()
-    for i in range(1, int(ceil(sqrt(n)))+1):
-        if n == (n / i) * i:
-            if i * i == n or i == 1:
+    for i in range(1, int(sqrt(n))+1):
+        if n % i == 0:
+            if i * i == n:
                 output.append(i)
             else:
                 output.append(i)
                 output.append(n/i)
-    output.append(n)
 
-    return sorted(list(set(output)))
+    return sorted(output)
