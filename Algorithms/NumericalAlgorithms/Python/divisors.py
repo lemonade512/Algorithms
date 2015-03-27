b0029e9338ce6a04d75d16325cd708849c219492
lemonade512 <philliplemons@utexas.edu>
2015-03-27 00:51:44 -0500

    Added documentation to divisors

--- Algorithms/NumericalAlgorithms/Python/divisors.py
+++ Algorithms/NumericalAlgorithms/Python/divisors.py
@@ -3,6 +3,20 @@
 from math import sqrt
 
 def divisors(n):
+    """ Calculates the divisors of `n` and returns the list.
+
+    Args:
+        n (int): The number to find the divisors of.
+
+    Returns:
+        A list of integers that n is divisible by including the integer
+        itself.
+
+    Example:
+        >>> divisors(28)
+        [1, 2, 4, 7, 14, 28]
+
+    """
     output = list()
     for i in range(1, int(sqrt(n))):
         if n == (n / i) * i:
